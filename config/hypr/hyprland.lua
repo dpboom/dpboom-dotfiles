7332a84bdf7c6f689e9cd67c977b325b83bdc2d0
dpboom <dpboom@users.noreply.github.com>
2026-08-05 06:56:00 +0300

    hyprland: use default monitor (output="", mode=preferred)

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -22,8 +22,8 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-  output   = "DP-1",
-  mode     = "1920x1080@200.00",
+  output   = "",
+  mode     = "preferred",
   position = "0x0",
   scale    = "1",
 })
